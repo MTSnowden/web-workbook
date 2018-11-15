aeef47c2f960eb5e6e6e8786a34acb2c2804a6ee
Michael Snowden <mtsnowden@gmail.com>
2018-11-14 20:59:20 -0600

    m

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,11 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+  window.onload = function(){
+    let list = document.getElementsByTagName('ul');
+    let message = "In yo buggie u got:" + list[0].children.length + "items.";
+    alert(message);
+
+    let newh2 = document.createElement('h2');
+    newh2.innerHTML= "U got" + count[0].children.length + "items in yo buggie";
+    document.getElementsByTagName('h1')[0].after(newh2);
+  }

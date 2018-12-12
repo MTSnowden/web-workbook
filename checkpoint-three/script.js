ac5a312dde3d693c647d1426cdcf19619cecc06e
Michael Snowden <mtsnowden@gmail.com>
2018-12-12 17:07:46 -0600

    final

--- checkpoint-three/script.js
+++ checkpoint-three/script.js
@@ -1,8 +1,65 @@
 'use strict';
+// onload alert
+alert("Let's count our eggs before they hatch!");
 
-var mikeSnow = "Michael Snowden";
+// mouseover mouseout 
+function bigImg(x) {
+    x.style.height = "600px";
+    x.style.width = "400px";
+}
+
+function normalImg(x) {
+    x.style.height = "300px";
+    x.style.width = "200px";
+}
 
 
+// counter
+var chicken = document.getElementById("chicken"),
+  count = 0;
+chicken.onclick = function() {
+  count += 1;
+  currentTotal.innerHTML = "Egg count: " + count;
+};
+
+
+$(document).ready(function () {
+    var num1 = 1;
+
+    $('#counter-1').click(function () {
+        num1++;
+        $(this).text(num1);
+    })
+});
+
+$(document).ready(function () {
+    var num2 = 10;
+
+    $('#counter-10').click(function () {
+        num2 += 10;
+        $(this).text(num2);
+    })
+});
+
+
+$(document).ready(function () {
+
+    var num3 = 100;
+    $('#counter-100').click(function () {
+        num3 += 100;
+        $(this).text(num3);
+    })
+});
+
+
+
+
+$('#counter-all').click(function () {
+
+    $('#counter-1').trigger('click');
+    $('#counter-10').trigger('click');
+    $('#counter-100').trigger('click');
+});
 
 
 
@@ -11,28 +68,28 @@ var mikeSnow = "Michael Snowden";
 //     totalCurrent:0, 
 //     totalRPS: 0
 //   };
-  
+
 //   setInterval(goGo,1000);
-  
+
 //   function goGo() {
 //     data.totalRevs += data.totalRPS;
 //     data.totalCurrent += data.totalRPS;
 //     $("#wheel").css({ 'transform': 'rotate(' + data.totalRevs + 'deg')});
 //     updateReport();
 //   }
-  
+
 //   function updateReport() {
 //     $("#currentTotal").text(Math.floor(data.totalCurrent));
 //     $("#rps").text((data.totalRPS/70.4).toFixed(3));
 //   }
-  
-  
+
+
 //   $("#wheel").click(function (){
 //     data.totalRevs ++;
 //     data.totalCurrent ++;
 //     updateReport();
 //   })
-  
+
 //   $(".button").click(function (){ 
 //     var addVal = $(this).data( "cost" );
 //     if ($(this).data( "cost" ) < data.totalCurrent ) {

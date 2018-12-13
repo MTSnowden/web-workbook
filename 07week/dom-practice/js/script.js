078b8affb27d3ea711ba3f607eef988386c1dd6d
Michael Snowden <mtsnowden@gmail.com>
2018-12-13 17:00:56 -0600

    in progress. can't get js to work

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -2,10 +2,23 @@
 
   window.onload = function(){
     let list = document.getElementsByTagName('ul');
-    let message = "In yo buggie u got:" + list[0].children.length + "items.";
-    alert(message);
+    let message = "In your buggie you have: " + list[0].children.length + " items.";
+    // alert(message);
 
     let newh2 = document.createElement('h2');
-    newh2.innerHTML= "U got" + count[0].children.length + "items in yo buggie";
+    newh2.innerHTML= "You have" + count[0].children.length + "items in your buggie";
     document.getElementsByTagName('h1')[0].after(newh2);
   }
+
+  function itemCount(){
+    let newh2 = document.createElement('h2');
+    newh2.setAttribute("id", "title");
+    document.getElementsByTagName('h1')[0].after(newh2);
+    }
+
+    function updateItemCount(){
+      let update = updateListCountSentance();
+      let string = document.getElementById('title');
+      string.innerHTML = update; 
+    }
+    
